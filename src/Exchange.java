83a5ea6fd6db6e5eddba8581ee7a059933917ff7
hyesam2 <hyesamsam@gmail.com>
2021-07-01 09:12:14 +0900

    Delete code of no use

--- src/Exchange.java
+++ src/Exchange.java
@@ -7,9 +7,9 @@ public class Exchange {
 		ExchangeUSD USD = new ExchangeUSD();
 		ExchangeEUR EUR = new ExchangeEUR();
 		ExchangeJPY JPY = new ExchangeJPY();
-		
+
 		int number;
-		
+
 		while (true) {
 			while (true) {
 				System.out.print("환전할 종류는? (1:USD, 2:EUR, 3:JPY) ");
@@ -20,7 +20,7 @@ public class Exchange {
 					break;
 				}
 			}
-		
+
 			switch (number) {
 			case 1:
 				USD.exchangeUSD();
@@ -31,14 +31,8 @@ public class Exchange {
 			case 3:
 				JPY.exchangeJPY();
 				break;
-			case 0:
-				System.out.println("환전 프로그램 종료");
-				return;
-			default:
-				break;
 			}
-			
-			
+
 			int terminate = sc.nextInt();
 			if (terminate != 0) {
 
